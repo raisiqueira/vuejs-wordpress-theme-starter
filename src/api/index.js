0fe6f16f7bdfe7cb3a8f8f9bc0d2ca980b14e872
Raí Siqueira <rai93siqueira@gmail.com>
2017-12-30 00:31:39 -0300

    change all endpoints and add getMenu method

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 export default {
   getCategories (cb) {
-    axios.get(window.SETTINGS.API_BASE_PATH + 'categories?sort=name&hide_empty=true&per_page=50')
+    axios.get(window.SETTINGS.API_BASE_PATH + 'wp/v2/categories?sort=name&hide_empty=true&per_page=50')
       .then(response => {
         cb(response.data.filter(c => c.name !== "Uncategorized"))
       })
@@ -10,7 +10,7 @@ export default {
   },
 
   getPages (cb) {
-    axios.get(window.SETTINGS.API_BASE_PATH + 'pages?per_page=10')
+    axios.get(window.SETTINGS.API_BASE_PATH + 'wp/v2/pages?per_page=10')
       .then(response => {
         cb(response.data)
       })
@@ -21,7 +21,7 @@ export default {
 
   getPage (id, cb) {
     if (_.isNull(id) || !_.isNumber(id)) return false
-    axios.get(window.SETTINGS.API_BASE_PATH + 'pages/'+id)
+    axios.get(window.SETTINGS.API_BASE_PATH + 'wp/v2/pages/'+id)
       .then(response => {
         cb(response.data)
       })
@@ -33,7 +33,7 @@ export default {
   getPosts (limit, cb) {
     if (_.isEmpty(limit)) { let limit = 5 }
     
-    axios.get(window.SETTINGS.API_BASE_PATH + 'posts?per_page='+limit)
+    axios.get(window.SETTINGS.API_BASE_PATH + 'wp/v2/posts?per_page='+limit)
       .then(response => {
         cb(response.data)
       })
@@ -41,4 +41,15 @@ export default {
         cb(e)
       })
   },
+
+  getMenu (slug, cb) {
+    if (_.isNull(id) || !_.isString(slug)) return false
+    axios.get(window.SETTINGS.API_BASE_PATH + 'menus/v1'+slug)
+    .then(response => {
+      cb(response.data)
+    })
+    .catch(e => {
+      cb(e)
+    })
+  }
 }
